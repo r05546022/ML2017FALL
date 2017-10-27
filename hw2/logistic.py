6d107856b8807692da60f15a1f996ac06ae58582
r05546022 <r05546022@ntu.edu.tw>
2017-10-27 23:50:19 +0800

    Add files via upload

--- hw2/logistic.py
+++ hw2/logistic.py
@@ -72,7 +72,7 @@ def train(X_all, Y_all, save_dir):
     w = np.zeros((106,))
     b = np.zeros((1,))
     l_rate = 0.001
-    batch_size = 40
+    batch_size = 32
     train_data_size = len(X_train)
     step_num = int(floor(train_data_size / batch_size))
     epoch_num = 1000
